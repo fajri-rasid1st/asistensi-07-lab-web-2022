fa4e5ea28700a53614cf3ead85294729f51185d2
MuhHudriPerdana <muhhudry18@gmail.com>
2022-11-12 16:51:54 +0800

    Tugas 7

---
+++ H071211042/Tugas 7/User.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY(id)
+);
